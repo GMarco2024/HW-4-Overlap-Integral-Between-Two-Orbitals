6bd0160096d87ae177e7c59edf424a35680211a2
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-03-08 11:35:11 -0600

    Changes were made.

--- Overlap Integral Between Two Orbitals/2px Orbital.swift
+++ Overlap Integral Between Two Orbitals/2px Orbital.swift
@@ -19,17 +19,26 @@
 
 import Foundation
 
-struct TwoPXOrbital {
-    
-    // Atomic number for hydrogen
+struct TwoPxOrbital {
+    // Atomic number for hydrogen-like orbitals
     let Z: Double = 1.0
-    
+
     // Bohr radius in angstroms
     let a: Double = 0.5292
-    
-    func calculateWavefunction(r: Double, theta: Double, phi: Double) -> Double {
-        let normalizationFactor = sqrt(4.0 * Z.pow(5) / (32.0 * Double.pi * a.pow(5)))
-        let radialPart = r * exp(-Z * r / (2 * a))
+
+    // Converts Cartesian coordinates to spherical (r, theta, phi)
+    func cartesianToSpherical(x: Double, y: Double, z: Double) -> (r: Double, theta: Double, phi: Double) {
+        let r = sqrt(x*x + y*y + z*z)
+        let theta = acos(z/r)
+        let phi = atan2(y, x)
+        return (r, theta, phi)
+    }
+
+    // Calculates the wavefunction value of a 2px orbital given a point in space (x, y, z)
+    func calculateWavefunction(x: Double, y: Double, z: Double) -> Double {
+        let (r, theta, phi) = cartesianToSpherical(x: x, y: y, z: z)
+        let normalizationFactor = 1.0 / sqrt(32.0 * Double.pi)
+        let radialPart = pow(Z / a, 2.5) * r * exp(-Z * r / (2 * a))
         let angularPart = sin(theta) * cos(phi)
         return normalizationFactor * radialPart * angularPart
     }
