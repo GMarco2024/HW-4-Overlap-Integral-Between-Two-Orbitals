009c101703fad6d0e99c117515b814ef8fa62e1b
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-03-08 13:38:25 -0600

    Changes were made.

--- Overlap Integral Between Two Orbitals/ViewModel.swift
+++ Overlap Integral Between Two Orbitals/ViewModel.swift
@@ -1,10 +1,3 @@
-//
-//  ViewModel.swift
-//  Overlap Integral Between Two Orbitals
-//
-//  Created by Marco on 3/4/24.
-//
-
 import Foundation
 
 class ViewModel: ObservableObject {
@@ -19,34 +12,45 @@ class ViewModel: ObservableObject {
     @Published var numberOfGuesses: String = "1000"
     
     @Published var selectedOrbitalTypeA: String = "1s"
-        @Published var selectedOrbitalTypeB: String = "1s"
-        let orbitalTypes = ["1s", "2px"]
+    @Published var selectedOrbitalTypeB: String = "1s"
+    let orbitalTypes = ["1s", "2px"]
     
-
     // Output properties
     @Published var monteCarloResult: Double = 0
     @Published var analyticalResult: Double = 0
 
     func calculateOverlapIntegral() {
         guard let xMax = Double(xMaxString), let xMin = Double(xMinString),
-            let yMax = Double(yMaxString), let yMin = Double(yMinString),
-            let zMax = Double(zMaxString), let zMin = Double(zMinString),
-            let R = Double(interatomicSpacing), let guesses = Int(numberOfGuesses) else {
-                return
+              let yMax = Double(yMaxString), let yMin = Double(yMinString),
+              let zMax = Double(zMaxString), let zMin = Double(zMinString),
+              let R = Double(interatomicSpacing), let guesses = Int(numberOfGuesses) else {
+            return
+        }
+
+        // Resetting results for each calculation
+        self.monteCarloResult = 0.0
+        self.analyticalResult = 0.0
+
+        // Check for 1s & 1s orbital overlap calculation
+        if selectedOrbitalTypeA == "1s" && selectedOrbitalTypeB == "1s" {
+            // Perform analytical calculation for 1s & 1s
+            self.analyticalResult = OrbitalCalculator.calculateOverlapIntegral(spacing: R)
+        }
+        
+        // Check for 2px & 2px orbital overlap calculation
+        else if selectedOrbitalTypeA == "2px" && selectedOrbitalTypeB == "2px" {
+            // Setup BoundingBox
+
+
+            let boundingBox = BoundingBox(xMin: xMin, xMax: xMax, yMin: yMin, yMax: yMax, zMin: zMin, zMax: zMax)
+            let monteCarlo = MonteCarlo(boundingBox: boundingBox, numberOfGuesses: guesses, interatomicSpacing: R)
             
+           // DON'T DELETE. This will be used in th far future.
+        //self.monteCarloResult = monteCarlo.integrateFor2px()
+            
+        } else {
+            // For combinations other than 1s & 1s or 2px & 2px.
+            print("Selected orbital combination calculation is not supported yet.")
         }
-                
-                // Reset results to default
-                self.monteCarloResult = 0.0  // Set to 0.0 because we are not performing Monte Carlo for other combinations or showing previous results
-                self.analyticalResult = 0.0  // Resetting analytical result as well
-
-                // Check if both selected orbitals are "1s"
-                if selectedOrbitalTypeA == "1s" && selectedOrbitalTypeB == "1s" {
-                    // Analytical Calculation only for 1s & 1s
-                    self.analyticalResult = OrbitalCalculator.calculateOverlapIntegral(spacing: R)
-                } else {
-                 
-                }
-            }
-        }
-
+    }
+}
